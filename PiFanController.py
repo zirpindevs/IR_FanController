9419fcfcfa4e3185454ee526c57949bb3cd36fb5
zirpin <zirpin@gmail.com>
2021-07-05 12:24:44 +0200

    initial commit

---
+++ PiFanController.py
@@ -0,0 +1,22 @@
+import time
+import serial
+
+
+ser = serial.Serial("/dev/ttyACM0", baudrate=9600) #Modificar el puerto serie de ser necesario
+
+try:
+    while True:
+        comando = input("Ingresar comando (on/off): ")
+        comando = comando + "\n"
+        comandoBytes = comando.encode()
+        ser.write(comandoBytes)
+        time.sleep(0.1)
+
+
+except KeyboardInterrupt:
+    print("\nInterrupcion por teclado")
+except ValueError as ve:
+    print(ve)
+    print("Otra interrupcion")
+finally:
+    ser.close()
