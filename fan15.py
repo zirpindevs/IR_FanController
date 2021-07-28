c631f6003964e5b4d68cc0b1a8e1f35e123232dc
zirpin <zirpin@gmail.com>
2021-07-28 13:26:19 +0200

    15

---
+++ fan15.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import os
+import time
+
+os.system('arduino-cli compile --fqbn arduino:avr:uno FanController_30Min/FanController_15Min.ino')
+
+time.sleep(1) #segundos
+
+os.system('arduino-cli upload -p /dev/ttyACM0 --fqbn arduino:avr:uno FanController_15Min')
--- fanON.py
+++ fanON.py
@@ -3,8 +3,8 @@
 import os
 import time
 
-#os.system('arduino-cli compile --fqbn arduino:avr:uno FanController_ON/FanController_ON.ino')
-#time.sleep(1) #segundos
+os.system('arduino-cli compile --fqbn arduino:avr:uno FanController_ON/FanController_ON.ino')
+time.sleep(1) #segundos
 
 os.system('arduino-cli upload -p /dev/ttyACM0 --fqbn arduino:avr:uno FanController_ON')
 
