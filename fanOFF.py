641963c92bb7364cf1b435df33911005ce2db323
zirpin <zirpin@gmail.com>
2021-07-28 11:54:08 +0200

    only upload not compile

--- fanOFF.py
+++ fanOFF.py
@@ -1,9 +1,8 @@
 import os
 import time
 
-os.system('arduino-cli compile --fqbn arduino:avr:uno FanController_OFF/FanController_OFF.ino')
-
-time.sleep(1) #segundos
+#os.system('arduino-cli compile --fqbn arduino:avr:uno FanController_OFF/FanController_OFF.ino')
+#time.sleep(1) #segundos
 
 os.system('arduino-cli upload -p /dev/ttyACM0 --fqbn arduino:avr:uno FanController_OFF')
 
