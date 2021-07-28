8797e5aa79cafd73cde48fa003f74e3a7bd328eb
zirpin <zirpin@gmail.com>
2021-07-28 14:45:10 +0200

    full path arduino cli

--- fanOFF.py
+++ fanOFF.py
@@ -1,7 +1,7 @@
 import os
 import time
 
-os.system('/home/pi/bin/arduino-cli compile --fqbn arduino:avr:uno FanController_OFF/FanController_OFF.ino')
+os.system('/home/pi/bin/arduino-cli compile --fqbn arduino:avr:uno /home/pi/IR_FanController/FanController_OFF/FanController_OFF.ino')
 time.sleep(1) #segundos
 
 os.system('/home/pi/bin/arduino-cli upload -p /dev/ttyACM0 --fqbn arduino:avr:uno FanController_OFF')
