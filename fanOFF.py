973d09e51ddcf39166a6325cef2ce7f360274f63
zirpin <zirpin@gmail.com>
2021-07-28 14:43:33 +0200

    full path arduino cli

--- fanOFF.py
+++ fanOFF.py
@@ -1,10 +1,10 @@
 import os
 import time
 
-os.system('arduino-cli compile --fqbn arduino:avr:uno FanController_OFF/FanController_OFF.ino')
+os.system('/home/pi/bin/arduino-cli compile --fqbn arduino:avr:uno FanController_OFF/FanController_OFF.ino')
 time.sleep(1) #segundos
 
-os.system('arduino-cli upload -p /dev/ttyACM0 --fqbn arduino:avr:uno FanController_OFF')
+os.system('/home/pi/bin/arduino-cli upload -p /dev/ttyACM0 --fqbn arduino:avr:uno FanController_OFF')
 
 
 # arduino-cli compile --fqbn arduino:avr:uno FanController_OFF.ino
--- fanON.py
+++ fanON.py
@@ -3,10 +3,10 @@
 import os
 import time
 
-os.system('arduino-cli compile --fqbn arduino:avr:uno FanController_ON/FanController_ON.ino')
+os.system('/home/pi/bin/arduino-cli compile --fqbn arduino:avr:uno FanController_ON/FanController_ON.ino')
 time.sleep(1) #segundos
 
-os.system('arduino-cli upload -p /dev/ttyACM0 --fqbn arduino:avr:uno FanController_ON')
+os.system('/home/pi/bin/arduino-cli upload -p /dev/ttyACM0 --fqbn arduino:avr:uno FanController_ON')
 
 
 # arduino-cli compile --fqbn arduino:avr:uno FanController_ON.ino
