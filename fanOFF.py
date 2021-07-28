d100f7a05ef64c605a8819e983b18c2ef93df684
zirpin <zirpin@gmail.com>
2021-07-28 11:48:42 +0200

    1

--- fanOFF.py
+++ fanOFF.py
@@ -3,7 +3,7 @@ import time
 
 os.system('arduino-cli compile --fqbn arduino:avr:uno FanController_OFF/FanController_OFF.ino')
 
-time.sleep(5) #segundos
+time.sleep(1) #segundos
 
 os.system('arduino-cli upload -p /dev/ttyACM0 --fqbn arduino:avr:uno FanController_OFF')
 
