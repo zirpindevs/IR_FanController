c0b63023e329c08c2f5210de67e84fe9a42e9959
zirpin <zirpin@gmail.com>
2021-07-28 14:46:26 +0200

    fix full path arduino cli

--- fanOFF.py
+++ fanOFF.py
@@ -4,7 +4,7 @@ import time
 os.system('/home/pi/bin/arduino-cli compile --fqbn arduino:avr:uno /home/pi/IR_FanController/FanController_OFF/FanController_OFF.ino')
 time.sleep(1) #segundos
 
-os.system('/home/pi/bin/arduino-cli upload -p /dev/ttyACM0 --fqbn arduino:avr:uno FanController_OFF')
+os.system('/home/pi/bin/arduino-cli upload -p /dev/ttyACM0 --fqbn arduino:avr:uno /home/pi/IR_FanController/FanController_OFF')
 
 
 # arduino-cli compile --fqbn arduino:avr:uno FanController_OFF.ino
--- fanON.py
+++ fanON.py
@@ -6,7 +6,7 @@ import time
 os.system('/home/pi/bin/arduino-cli compile --fqbn arduino:avr:uno /home/pi/IR_FanController/FanController_ON/FanController_ON.ino')
 time.sleep(1) #segundos
 
-os.system('/home/pi/bin/arduino-cli upload -p /dev/ttyACM0 --fqbn arduino:avr:uno FanController_ON')
+os.system('/home/pi/bin/arduino-cli upload -p /dev/ttyACM0 --fqbn arduino:avr:uno /home/pi/IR_FanController/FanController_ON')
 
 
 # arduino-cli compile --fqbn arduino:avr:uno FanController_ON.ino
