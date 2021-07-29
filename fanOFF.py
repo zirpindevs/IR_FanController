07acc10d4cbd91839992383a387ff653e10a53a1
zirpin <zirpin@gmail.com>
2021-07-29 10:01:16 +0200

    test bullet

--- fanOFF.py
+++ fanOFF.py
@@ -1,12 +1,31 @@
 import os
 import time
+import MySQLdb
+import requests
+import json
+
+api_key = "o.fUycrvSsqihUVsNCgj0MWDOwIxitAKLb"
+
+def pushbullet(cfg, msg):
+    try:
+        data_send = {"type": "note", "title": "FANbot", "body": msg}
+        requests.post(
+            'https://api.pushbullet.com/v2/pushes',
+            data=json.dumps(data_send),
+            headers={'Authorization': 'Bearer ' + cfg,
+                     'Content-Type': 'application/json'})
+    except (KeyboardInterrupt, SystemExit):
+        raise
 
 os.system('/home/pi/bin/arduino-cli compile --fqbn arduino:avr:uno /home/pi/IR_FanController/FanController_OFF/FanController_OFF.ino')
+
 time.sleep(1) #segundos
 
 os.system('/home/pi/bin/arduino-cli upload -p /dev/ttyACM0 --fqbn arduino:avr:uno /home/pi/IR_FanController/FanController_OFF')
 
 print("fan off")
+
+pushbullet(api_key, "FAN| OFF ")
 
 # arduino-cli compile --fqbn arduino:avr:uno FanController_OFF.ino
 
--- fanON.py
+++ fanON.py
@@ -2,13 +2,33 @@
 # -*- coding: utf-8 -*-
 import os
 import time
+import pymysql
+import requests
+import json
+
+# Pusshbullet api key
+api_key = "o.fUycrvSsqihUVsNCgj0MWDOwIxitAKLb"
+
+def pushbullet(cfg, msg):
+    try:
+        data_send = {"type": "note", "title": "FANbot", "body": msg}
+        requests.post(
+            'https://api.pushbullet.com/v2/pushes',
+            data=json.dumps(data_send),
+            headers={'Authorization': 'Bearer ' + cfg,
+                     'Content-Type': 'application/json'})
+    except (KeyboardInterrupt, SystemExit):
+        raise
+
 
 os.system('/home/pi/bin/arduino-cli compile --fqbn arduino:avr:uno /home/pi/IR_FanController/FanController_ON/FanController_ON.ino')
 time.sleep(1) #segundos
 
 os.system('/home/pi/bin/arduino-cli upload -p /dev/ttyACM0 --fqbn arduino:avr:uno /home/pi/IR_FanController/FanController_ON')
 
-print("fan off")
+pushbullet(api_key, "FAN| ON ")
+
+print("fan on")
 
 # arduino-cli compile --fqbn arduino:avr:uno FanController_ON.ino
 
