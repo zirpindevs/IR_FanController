227dbaccba70208ba1bf4e57a2a75e33b830389b
zirpin <zirpin@gmail.com>
2021-07-29 11:56:21 +0200

    obtain data config file and write bbdd

--- fanOFF.py
+++ fanOFF.py
@@ -1,14 +1,42 @@
+import configparser
 import os
 import time
 import MySQLdb
 import requests
 import json
 
-api_key = "o.fUycrvSsqihUVsNCgj0MWDOwIxitAKLb"
 
+# Function insert luz and vent , alarma and calentador status to table
+def set_status_fan(status):
+    config = configparser.ConfigParser()
+    config.read('/home/pi/config.ini')
 
-def pushbullet(cfg, msg):
+    host = config['default']['host']
+    username = config['default']['username']
+    password = config['default']['password']
+    db_fan = config['default']['db_fan']
+
+    # connect to db
+    db = MySQLdb.connect(host, username, password, db_fan)
+    # setup cursor
+    cursor = db.cursor()
     try:
+        cursor.execute(
+            """INSERT INTO status_fan (estado) VALUES (%s)""",
+            status)
+        db.commit()
+        print("insert OK")
+    except:
+        db.rollback()
+        print("error inserting status gpio")
+
+
+def pushbullet(msg):
+    try:
+        config = configparser.ConfigParser()
+        config.read('/home/pi/config.ini')
+        cfg = config['default']['api_key']
+
         data_send = {"type": "note", "title": "FANbot", "body": msg}
         requests.post(
             'https://api.pushbullet.com/v2/pushes',
@@ -20,15 +48,18 @@ def pushbullet(cfg, msg):
 
 
 def main():
-    os.system('/home/pi/bin/arduino-cli compile --fqbn arduino:avr:uno /home/pi/IR_FanController/FanController_OFF/FanController_OFF.ino')
+    os.system(
+        '/home/pi/bin/arduino-cli compile --fqbn arduino:avr:uno /home/pi/IR_FanController/FanController_OFF/FanController_OFF.ino')
 
-    time.sleep(1) #segundos
+    time.sleep(1)  # segundos
 
-    os.system('/home/pi/bin/arduino-cli upload -p /dev/ttyACM0 --fqbn arduino:avr:uno /home/pi/IR_FanController/FanController_OFF')
+    os.system(
+        '/home/pi/bin/arduino-cli upload -p /dev/ttyACM0 --fqbn arduino:avr:uno /home/pi/IR_FanController/FanController_OFF')
 
     print("fan off")
 
-    pushbullet(api_key, "FAN| OFF ")
+    pushbullet("FAN| OFF ")
+
 
 # arduino-cli compile --fqbn arduino:avr:uno FanController_OFF.ino
 
