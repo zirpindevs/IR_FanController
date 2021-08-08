2c530600ac092b43e6388a7998112c9a2500b6d6
zirpin <zirpin@gmail.com>
2021-08-08 21:16:58 +0200

    activar compilado

--- fan30.py
+++ fan30.py
@@ -26,9 +26,9 @@ def pushbullet(msg):
 
 
 def main():
-   # os.system('/home/pi/bin/arduino-cli compile --fqbn arduino:avr:uno /home/pi/IR_FanController/FanController_30Min/FanController_30Min.ino')
+    os.system('/home/pi/bin/arduino-cli compile --fqbn arduino:avr:uno /home/pi/IR_FanController/FanController_30Min/FanController_30Min.ino')
 
-    #time.sleep(1) #segundos
+    time.sleep(1) #segundos
 
     os.system('/home/pi/bin/arduino-cli upload -p /dev/ttyACM0 --fqbn arduino:avr:uno /home/pi/IR_FanController/FanController_30Min')
 
